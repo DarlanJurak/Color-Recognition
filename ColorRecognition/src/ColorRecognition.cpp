cf8b6656a8af9b597f335e7e32041e4bca3bd7c3
Darlan <darlan.jurak@gmail.com>
2017-05-16 22:24:40 -0300

    Added initial color detection test (red color)

---
+++ ColorRecognition/src/ColorRecognition.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(){
+
+	Mat image = imread("repo/img/RedBall.png", 1);
+	namedWindow("Image", CV_WINDOW_FREERATIO);
+	imshow("Image", image);
+	waitKey(0);
+
+	//Blue Green Red
+	Mat outputImg;
+	inRange(image, Scalar(10, 10, 100), Scalar(100, 100, 255), outputImg);
+
+	namedWindow("Output", CV_WINDOW_FREERATIO);
+	imshow("Output", outputImg);
+	waitKey(0);
+
+	cout << "Hello World" << endl;
+
+
+	return 0;
+}
